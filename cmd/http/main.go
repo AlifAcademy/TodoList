9ab9290a210d0d95aa08436b43db760d97b05723
adheeeem <sherman.peabody99@gmail.com>
2023-05-05 09:04:19 +0500

    added feature logger

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/AlifAcademy/TodoList/internal/logger"
+	log "github.com/sirupsen/logrus"
+)
+
 func main() {
-	
+	logger.NewLogger()
+	log.WithFields(log.Fields{
+		"test": "another thing",
+	}).Info("Just a test message 123")
 }
