e25a18b451a74619c9eda6cd3cd7ec358ce45636
Akbar <Akbarjon@go.alif.academy>
2023-05-18 20:05:57 +0500

    fixed logger modul

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -10,6 +10,7 @@ import (
 	"syscall"
 
 	"github.com/AlifAcademy/TodoList/config"
+	"github.com/AlifAcademy/TodoList/internal/logger"
 	"github.com/AlifAcademy/TodoList/internal/server"
 	"github.com/gorilla/mux"
 	"go.uber.org/dig"
@@ -19,6 +20,7 @@ func main() {
 	
 	deps := []interface{}{
 		config.New,
+		logger.NewLogger,
 		server.NewServer,
 		mux.NewRouter,
 		serverInit,
@@ -48,18 +50,19 @@ func main() {
 		return server.ListenAndServe()
 	})
 
-	container.Invoke(func(server *http.Server) error {
+	container.Invoke(func(logger logger.Logger, server *http.Server) error {
 		quit := make(chan os.Signal, 1)
  	 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
   		<-quit
 
   		fmt.Println("TodoList Shutting Down")
+		
 		return server.Shutdown(context.TODO())
 	})
 	
 }
 
-func serverInit(server *server.Server, config config.Config) *http.Server{
+func serverInit(server *server.Server, config config.Config, logger logger.Logger) *http.Server{
 	return &http.Server{
 		Addr: net.JoinHostPort(
 			config.GetString("todo.host"),
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,8 +2,14 @@ package logger
 
 import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	FOLDER_LOG_NAME  = "log"
+	FILE_NAME = "log.log"
 )
 
 // Logger interface
@@ -13,44 +19,58 @@ type Logger interface {
 }
 
 // FileLogger logs inside the file
-type FileLogger struct {
-	filename string
+type fileLogger struct {
+	logrus *log.Logger
+	file *os.File
 }
 
 // NewFileLogger creates a new instance of FileLogger
-func NewFileLogger(filename string) *FileLogger {
-	return &FileLogger{
-		filename: filename,
+func NewLogger() (Logger, error) {
+	_, err := os.Stat(FOLDER_LOG_NAME);
+	if err != nil {
+		os.Mkdir(FOLDER_LOG_NAME, 0666)	
 	}
+
+	file, err := os.OpenFile(FOLDER_LOG_NAME +"/"+ FILE_NAME, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	// defer file.Close()
+	logger := log.New()
+	logger.SetOutput(file)
+	logger.SetFormatter(&log.JSONFormatter{})
+	
+	item := &fileLogger{
+		logrus: logger,
+		file: file,
+	}
+
+	return item, nil
 }
 
 // Print method outputs an appropriate log according to the level
-func (f *FileLogger) Print(level log.Level, message string) {
-	file, err := os.OpenFile(f.filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer file.Close()
-
-	log.SetOutput(file)
-	log.SetFormatter(&log.JSONFormatter{})
-	log.SetLevel(level)
-
+func (f *fileLogger) Print(level log.Level, message string) {
+	
+	f.logrus.SetLevel(level)
 	switch level {
 	case log.InfoLevel:
-		log.WithFields(log.Fields{}).Info(message)
+		f.logrus.WithFields(log.Fields{}).Info(message)
 	case log.ErrorLevel:
-		log.WithFields(log.Fields{}).Error(message)
+		f.logrus.WithFields(log.Fields{}).Error(message)
 	}
 }
 
 // Info method just logs a message
-func (f *FileLogger) Info(message string) {
+func (f *fileLogger) Info(message string) {
 	f.Print(log.InfoLevel, message)
 }
 
-func (f *FileLogger) Error(err error) {
+func (f *fileLogger) Error(err error) {
 	f.Print(log.ErrorLevel, err.Error())
+}
+
+func (f *fileLogger) FileClose() {
+	f.file.Close()
 }
