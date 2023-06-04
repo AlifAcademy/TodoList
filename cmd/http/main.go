979273f5c2e1489e07c9b710fdc57d8512e18874
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-06-04 19:59:40 +0500

    Run

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,5 +1,67 @@
 package main
 
+import (
+	"net"
+	"net/http"
+
+	"github.com/AlifAcademy/TodoList/config"
+	"github.com/AlifAcademy/TodoList/internal/db/postgres"
+	"github.com/AlifAcademy/TodoList/internal/logger"
+	"github.com/AlifAcademy/TodoList/internal/server"
+	"github.com/gorilla/mux"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"go.uber.org/dig"
+)
+
 func main() {
+	log := logger.NewFileLogger("logs.log")
 	
+
+	postgres.NewPostgresDB(config.New())
+
+	deps := []interface{}{
+		config.New,
+		server.NewServer,
+		mux.NewRouter,
+		logger.NewFileLogger,
+		serverInit,
+	}
+	
+	container := dig.New()
+	for _, dep := range deps {
+		err := container.Provide(dep)
+		if err != nil {
+			log.Error(err)
+			return 
+		}
+	}
+
+	err := container.Invoke(func(server *server.Server) {
+		server.Init()
+	})
+
+	if err != nil {
+		log.Error(err)
+		return 
+	}
+	
+	
+	container.Invoke(func(server *http.Server) error {
+		log.Info("Todo list starting")
+		return server.ListenAndServe()
+	})
+	
+
+}
+
+
+func serverInit(server *server.Server, config config.Config) *http.Server{
+	return &http.Server{
+		Addr: net.JoinHostPort(
+			config.GetString("todo.host"),
+			config.GetString("todo.port"),
+			),
+		Handler: server,
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,27 @@
 package server
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+)
+// Server type created
+type Server struct {
+	mux    *mux.Router
+	//config *config.Config
+}
+
+// NewServer constructor
+func NewServer(mux *mux.Router) *Server {
+	return &Server{mux: mux}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mux.ServeHTTP(w, r)
+}
+
+// Init Server initialization
+func (s *Server) Init() {
+	s.mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+}
