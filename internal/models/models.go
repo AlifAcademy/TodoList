21587cbd8ffa583d08972ea57bee0716d59fdfb0
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-06-08 12:10:05 +0500

    Code refactoring

---
+++ internal/models/models.go
@@ -0,0 +1,39 @@
+package models
+
+import "time"
+
+// User type
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password,omitempty"`
+	Hash     string `json:"password_hash"`
+}
+
+// Task type
+type Task struct {
+	ID          int64     `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Tags        []string  `json:"tags"`
+	StatusID    int64     `json:"status_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	UserID      int64     `json:"user_id"`
+}
+
+// Comment type
+type Comment struct {
+	ID        int64     `json:"id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	TaskID    int64     `json:"task_id"`
+	UserID    int64     `json:"user_id"`
+}
+
+// TagStatus type
+type TagStatus struct {
+	Tag    string `json:"tag"`
+	Status string `json:"status"`
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,15 +3,17 @@ package server
 import (
 	"encoding/json"
 	"errors"
-	"log"
-	"net/http"
-	"strconv"
 	"github.com/AlifAcademy/TodoList/internal/logger"
 	"github.com/AlifAcademy/TodoList/internal/middleware"
+	"github.com/AlifAcademy/TodoList/internal/models"
 	"github.com/AlifAcademy/TodoList/internal/service"
 	"github.com/AlifAcademy/TodoList/internal/service/security"
 	"github.com/AlifAcademy/TodoList/pkg/types"
 	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 var lg = logger.NewFileLogger("logs.log")
@@ -67,33 +69,24 @@ func (s *Server) Init() {
 }
 
 func (s *Server) handleNewUser(writer http.ResponseWriter, request *http.Request) {
-	var user *service.User
+	var user *models.User
 
 	err := json.NewDecoder(request.Body).Decode(&user)
+	writer.Header().Set("Content-Type", "application/json")
 
 	if err != nil {
-		lg.Error(err)
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		writer.Write(models.ResponseError(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).ToBytes())
 		return
 	}
 
 	items, err := s.userSvc.NewUser(request.Context(), user)
 
-	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
-	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+
+	_, err = writer.Write(models.ResponseWrite("New User Successfully Created!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -102,33 +95,27 @@ func (s *Server) handleNewUser(writer http.ResponseWriter, request *http.Request
 }
 
 func (s *Server) handleNewTask(writer http.ResponseWriter, request *http.Request) {
-	var task *service.Task
+	writer.Header().Set("Content-Type", "application/json")
+
+	var task *models.Task
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
 	err := json.NewDecoder(request.Body).Decode(&task)
+
 	if err != nil {
-		lg.Error(err)
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		writer.Write(models.ResponseError(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).ToBytes())
 		return
 	}
+	task.CreatedAt = time.Now()
+	task.UpdatedAt = time.Now()
 
 	items, err := s.userSvc.NewTask(request.Context(), task, userID)
 
-	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
-	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+	_, err = writer.Write(models.ResponseWrite("New Task Successfully Created!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -137,9 +124,11 @@ func (s *Server) handleNewTask(writer http.ResponseWriter, request *http.Request
 }
 
 func (s *Server) handleDeleteTaskByID(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
 	idParam, ok := mux.Vars(request)["id"]
 	if !ok {
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		writer.Write(models.ResponseError(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).ToBytes())
 		return
 	}
 	id, err := strconv.ParseInt(idParam, 10, 64)
@@ -148,23 +137,18 @@ func (s *Server) handleDeleteTaskByID(writer http.ResponseWriter, request *http.
 	}
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
-	
+
 	items, err := s.userSvc.DeleteTaskByID(request.Context(), id, userID)
 	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		writer.Write(models.ResponseError(http.StatusNotFound, "Task Not Found").ToBytes())
 		return
 	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+
+	_, err = writer.Write(models.ResponseWrite("Task Successfully Deleted!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -172,31 +156,27 @@ func (s *Server) handleDeleteTaskByID(writer http.ResponseWriter, request *http.
 	}
 }
 
-func (s *Server) handleGetAllTasks(writer http.ResponseWriter, request *http.Request)  {
+func (s *Server) handleGetAllTasks(writer http.ResponseWriter, request *http.Request) {
 	status := request.URL.Query().Get("status")
 	tag := request.URL.Query().Get("tag")
 	searchText := request.URL.Query().Get("search")
 	log.Print(tag)
 
+	writer.Header().Set("Content-Type", "application/json")
+
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
-	
+
 	items, err := s.userSvc.GetAllTasks(request.Context(), userID, tag, status, searchText)
 	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		writer.Write(models.ResponseError(http.StatusNotFound, http.StatusText(http.StatusNotFound)).ToBytes())
 		return
 	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+	_, err = writer.Write(models.ResponseWrite("Tasks successfully retrieved!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -205,33 +185,32 @@ func (s *Server) handleGetAllTasks(writer http.ResponseWriter, request *http.Req
 }
 
 func (s *Server) handleUpdateTask(writer http.ResponseWriter, request *http.Request) {
-	var task *service.Task
+	var task *models.Task
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
 	err := json.NewDecoder(request.Body).Decode(&task)
+
+	writer.Header().Set("Content-Type", "application/json")
+
 	if err != nil {
 		lg.Error(err)
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		writer.Write(models.ResponseError(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).ToBytes())
 		return
 	}
+
+	task.UpdatedAt = time.Now()
 
 	items, err := s.userSvc.UpdateTask(request.Context(), task, userID)
 
 	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		writer.Write(models.ResponseError(http.StatusNotFound, http.StatusText(http.StatusNotFound)).ToBytes())
 		return
 	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+	_, err = writer.Write(models.ResponseWrite("Task successfully updated!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -240,34 +219,31 @@ func (s *Server) handleUpdateTask(writer http.ResponseWriter, request *http.Requ
 }
 
 func (s *Server) handleMarkTaskAsCompeted(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
 	idParam, ok := mux.Vars(request)["id"]
 	if !ok {
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		writer.Write(models.ResponseError(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).ToBytes())
 		return
 	}
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
 		lg.Error(err)
+		return
 	}
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
-	
+
 	items, err := s.userSvc.MarkAsCompleted(request.Context(), id, userID)
 	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		writer.Write(models.ResponseError(http.StatusNotFound, http.StatusText(http.StatusNotFound)).ToBytes())
 		return
 	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+	_, err = writer.Write(models.ResponseWrite("Task marked as completed!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -276,34 +252,31 @@ func (s *Server) handleMarkTaskAsCompeted(writer http.ResponseWriter, request *h
 }
 
 func (s *Server) handleMarkTaskAsCanceled(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
 	idParam, ok := mux.Vars(request)["id"]
 	if !ok {
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		writer.Write(models.ResponseError(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).ToBytes())
 		return
 	}
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
 		lg.Error(err)
+		return
 	}
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
-	
+
 	items, err := s.userSvc.MarkAsCanceled(request.Context(), id, userID)
 	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		writer.Write(models.ResponseError(http.StatusNotFound, http.StatusText(http.StatusNotFound)).ToBytes())
 		return
 	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+	_, err = writer.Write(models.ResponseWrite("Task marked as canceled!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -312,33 +285,32 @@ func (s *Server) handleMarkTaskAsCanceled(writer http.ResponseWriter, request *h
 }
 
 func (s *Server) handleAddComment(writer http.ResponseWriter, request *http.Request) {
-	var comment *service.Comment
+	writer.Header().Set("Content-Type", "application/json")
+
+	var comment *models.Comment
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
 	err := json.NewDecoder(request.Body).Decode(&comment)
 	if err != nil {
 		lg.Error(err)
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		writer.Write(models.ResponseError(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).ToBytes())
 		return
 	}
+
+	comment.CreatedAt = time.Now()
 
 	items, err := s.userSvc.AddComment(request.Context(), comment, userID)
 
 	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		writer.Write(models.ResponseError(http.StatusNotFound, http.StatusText(http.StatusNotFound)).ToBytes())
 		return
 	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+
+	_, err = writer.Write(models.ResponseWrite("Comment added successfully!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -346,26 +318,23 @@ func (s *Server) handleAddComment(writer http.ResponseWriter, request *http.Requ
 	}
 }
 
-func (s *Server) handleGetUser(writer http.ResponseWriter, request *http.Request)  {
+func (s *Server) handleGetUser(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
-	
+
 	items, err := s.userSvc.GetUserInfo(request.Context(), userID)
-	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	if errors.Is(err, service.ErrNoSuchUser) {
+		writer.Write(models.ResponseError(http.StatusNotFound, http.StatusText(http.StatusNotFound)).ToBytes())
 		return
 	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+
+	_, err = writer.Write(models.ResponseWrite("User info retrieved successfully!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
@@ -373,30 +342,25 @@ func (s *Server) handleGetUser(writer http.ResponseWriter, request *http.Request
 	}
 }
 
-func (s *Server) handleGetStatusAndTag(writer http.ResponseWriter, request *http.Request)  {
+func (s *Server) handleGetStatusAndTag(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
 	value := request.Context().Value(types.Key("key"))
 	userID := value.(int64)
-	
+
 	items, err := s.userSvc.GetStatusAndTag(request.Context(), userID)
 	if errors.Is(err, service.ErrNotFound) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		writer.Write(models.ResponseError(http.StatusNotFound, http.StatusText(http.StatusNotFound)).ToBytes())
 		return
 	}
 	if err != nil {
-		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		writer.Write(models.ResponseError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)).ToBytes())
 		return
 	}
-	data, err := json.Marshal(items)
-	if err != nil {
-		lg.Error(err)
-		return
-	}
-	writer.Header().Set("Content-Type", "application/json")
-	_, err = writer.Write(data)
+	_, err = writer.Write(models.ResponseWrite("Tags and Statuses retrieved successfully!", items).ToBytes())
 
 	if err != nil {
 		lg.Error(err)
 		return
 	}
 }
-
